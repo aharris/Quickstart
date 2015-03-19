a444811c26a972aef32b642480f456378d1cf14a
Ashton Harris <ashtonharris.me@gmail.com>
2015-03-19 02:29:59 -0400

    Fixed gulpfile indentation

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,24 @@
 var gulp = require('gulp'),
-    stylus = require('gulp-stylus'),
-    nib = require('nib'),
-    jeet = require('jeet'),
-    watch = require('gulp-watch'),
-    connect = require('gulp-connect'),
-    clean = require('gulp-clean'),
-    runSequence = require('run-sequence'),
-    rupture = require('rupture'),
-    jade = require('gulp-jade'),
-    shell = require('gulp-shell');
+  stylus = require('gulp-stylus'),
+  nib = require('nib'),
+  jeet = require('jeet'),
+  watch = require('gulp-watch'),
+  connect = require('gulp-connect'),
+  clean = require('gulp-clean'),
+  runSequence = require('run-sequence'),
+  rupture = require('rupture'),
+  jade = require('gulp-jade'),
+  shell = require('gulp-shell');
 
 gulp.task('stylus', function () {
-    gulp.src(['./styl/**/*.styl', '!styl/**/_*'])
-        .pipe(stylus({use: [nib(), jeet(), rupture()]}))
-        .pipe(gulp.dest('./css'))
-        .pipe(connect.reload());
+  gulp.src(['./styl/**/*.styl', '!styl/**/_*'])
+    .pipe(stylus({use: [nib(), jeet(), rupture()]}))
+    .pipe(gulp.dest('./css'))
+    .pipe(connect.reload());
 });
 
 gulp.task('stylint', shell.task([
-    'stylint ./styl/ -c .stylintrc'
+  'stylint ./styl/ -c .stylintrc'
 ]));
 
 gulp.task('jade', function() {
@@ -30,32 +30,32 @@ gulp.task('jade', function() {
 });
 
 gulp.task('html', function () {
-    gulp.src('**/*.html')
-      .pipe(connect.reload());
+  gulp.src('**/*.html')
+    .pipe(connect.reload());
 });
 
 gulp.task('js', function () {
-    gulp.src('./js/*.js')
-      .pipe(connect.reload());
+  gulp.src('./js/*.js')
+    .pipe(connect.reload());
 });
 
 gulp.task('watch', function () {
-    gulp.watch(['styl/**/*.styl'], ['stylus', 'stylint']);
-    gulp.watch(['templates/**/*.jade'], ['jade']);
-    gulp.watch(['./**/*.html'], ['html']);
-    gulp.watch(['./js/*.js'], ['js']);
+  gulp.watch(['styl/**/*.styl'], ['stylus', 'stylint']);
+  gulp.watch(['templates/**/*.jade'], ['jade']);
+  gulp.watch(['./**/*.html'], ['html']);
+  gulp.watch(['./js/*.js'], ['js']);
 });
 
 gulp.task('connect', function() {
-    connect.server({
-        root: [__dirname],
-        livereload: true
-    });
+  connect.server({
+    root: [__dirname],
+    livereload: true
+  });
 });
 
 gulp.task('clean', function () {
-    return gulp.src('build', {read: false})
-        .pipe(clean());
+  return gulp.src('build', {read: false})
+    .pipe(clean());
 });
 
 gulp.task('copy', function() {
@@ -86,8 +86,8 @@ gulp.task('build', function(callback){
 });
 
 gulp.task('default', [
-    'stylus',
-    'jade',
-    'connect',
-    'watch'
+  'stylus',
+  'jade',
+  'connect',
+  'watch'
 ]);
